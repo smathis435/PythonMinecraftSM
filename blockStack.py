72b60718be0366dcb1632735f024bbcd562d177c
dkascsakhoaglin660 <dkascsakhoaglin660@lsr7.net>
2021-11-22 13:57:08 -0600

    adding chapter 3

--- blockStack.py
+++ blockStack.py
@@ -1,7 +1,10 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
-x = 5
+x = 2
 y = 30
-z = 25
+z = 28
 blockType = 103
+mc.setBlock(x, y, z, blockType)
+
+y = y + 1
 mc.setBlock(x, y, z, blockType)
